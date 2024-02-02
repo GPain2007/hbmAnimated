7860e3f1eaf4731aee2270c0df168c0b04118852
GenThrive <genthrive@ecorise.org>
2024-02-01 22:03:58 -0600

    made changes to the pages

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import "./testimonial.scss";
 import { TestmonialData } from "./ImageData";
+import Logo from "/newlogo.png";
+import Icon from "/testimonial-icons-thumbnail.png";
 
 const Testimonial = () => {
   const [currentQuote, setCurrentQuote] = useState(0);
@@ -15,12 +18,71 @@ const Testimonial = () => {
     }, 5000);
   }, [currentQuote]);
 
+  const testVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1,
+      },
+    },
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+
+  const sliderVariants = {
+    initial: {
+      x: 0,
+    },
+
+    animate: {
+      x: "-220%",
+      transition: {
+        repeat: Infinity,
+        repeatType: "mirror",
+        duration: 20,
+      },
+    },
+  };
+
+  const carouselVariants = {
+    initial: {
+      x: +500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
   return (
     <div>
       <section className="testimonial">
-        <div className="pageTitle">
-          <h1>Kind Words</h1>
-        </div>
+        <motion.div
+          className="pageTitle"
+          variants={testVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h1 variants={testVariants}>Kind Words</motion.h1>
+        </motion.div>
+
         {TestmonialData.map((slide, index) => {
           const indexLeft = mod(currentQuote - 1, TestmonialData.length);
           const indexRight = mod(currentQuote + 1, TestmonialData.length);

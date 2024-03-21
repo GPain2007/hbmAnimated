0e5954187d6aede8d96ff7b73ae5453878fe0421
GenThrive <genthrive@ecorise.org>
2024-03-21 12:09:54 -0500

    added contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
+import { Container, Button } from "react-bootstrap";
 import { NavBar } from "../../components/navbar/NavBar";
+import ContactForm from "../../components/contactForm/ContactForm";
+
+import "./Contact.scss";
 
 const Contact = () => {
+  const [modalShow, setModalShow] = useState(false);
   return (
     <div>
       <section>
         <NavBar />
-        Contact
+        <div className="contact_page text-center">
+          <ContactForm show={modalShow} onHide={() => setModalShow(false)} />
+          <div
+            className="contact_button" // Use your custom class
+            onClick={() => setModalShow(true)}
+          >
+            Contact US
+          </div>
+        </div>
       </section>
     </div>
   );

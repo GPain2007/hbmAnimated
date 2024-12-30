f03b9a8a837b516939cb64db2fe184c6d90c72c1
GenThrive <genthrive@ecorise.org>
2024-12-30 17:06:19 -0600

    fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,29 +8,20 @@ const Footer = () => {
         <h1>{`Let's Create Together`}</h1>
         <p>Want to work with HBM Flower City?</p>
         <p>
-          Send us an email at&nbsp;
-          <a href="mailto:hello@hbmflower-city.com">hello@hbmflower-city.com</a>
-          &nbsp;about your floral needs or click the link to tell us about your
-          event.
+          Send us an email at
+          <a href="mailto:hello@hbmflower-city.com">
+            &nbsp;hello@hbmflower-city.com&nbsp;
+          </a>
+          about your floral needs or click <br />
+          the link to tell us about your event.
         </p>
-        <a href="/contact">
-          <button>Inquire</button>
-        </a>
+
         <a
           href="https://www.southernbride.com/blog/inspiration/timeless-elegance-a-classic-blue-white-chinoiserie-inspired-styled-shoot-in-boerne-tx/?swcfpc=1&fbclid=PAZXh0bgNhZW0CMTEAAaZ-b0JkgL1nHnxgEBhsG_yGjsBfNqRq24d-V1mlTR7ETkpB4hwCnrHHy_s_aem_hXL1qyaBnstjayL4dFnx0A"
           target="_blank"
           rel="noreferrer"
         >
-          <img
-            src={IMG}
-            alt="Southern Bride"
-            style={{
-              height: "220px",
-              width: "220px",
-              alignSelf: "flex-end",
-              marginLeft: "68rem",
-            }}
-          />
+          <img src={IMG} alt="Southern Bride" className="southernBride" />
         </a>
       </div>
     </div>
